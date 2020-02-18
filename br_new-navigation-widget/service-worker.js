cec0fb069522543bb360e30d68400f3f22079939
shayan <shayan-binary-2@protonmail.com>
2020-02-18 07:04:18 +0330

    Updates

--- br_new-navigation-widget/service-worker.js
+++ br_new-navigation-widget/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/br_new-navigation-widget/precache-manifest.3fbec8a2c0e256a4893dae9f95e54eb3.js"
+  "/br_new-navigation-widget/precache-manifest.ffaf74c1210eb5e2635f42f32d184e3c.js"
 );
 
 workbox.core.skipWaiting();
