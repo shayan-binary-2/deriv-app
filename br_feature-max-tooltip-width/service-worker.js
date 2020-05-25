a938e41ebb61a3431810c946022b4e22757f293e
shayan <shayan@firstsource.tech>
2020-05-25 17:09:03 +0430

    Updates

--- br_feature-max-tooltip-width/service-worker.js
+++ br_feature-max-tooltip-width/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/br_feature-max-tooltip-width/precache-manifest.367ac36f262cd021b8cf85e65d6bb3db.js"
+  "/br_feature-max-tooltip-width/precache-manifest.7d76fbacece456fb81ef11647a73930c.js"
 );
 
 workbox.core.skipWaiting();
