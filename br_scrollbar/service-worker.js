39a70e7e1b365d7bf32d6f13817cf8ac28833aad
shayan <shayan@firstsource.tech>
2020-06-15 14:45:31 +0430

    Updates

--- br_scrollbar/service-worker.js
+++ br_scrollbar/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/br_scrollbar/precache-manifest.6b77a8b2cbaae09482a2cfea9b151d9d.js"
+  "/br_scrollbar/precache-manifest.55b91096dc46af793c6cfda2ad00cd4d.js"
 );
 
 workbox.core.skipWaiting();
