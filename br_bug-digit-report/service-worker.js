ba731c70326116a6d33d19e90bf6ec1e2eae91f8
shayan <shayan@firstsource.tech>
2020-06-18 08:32:53 +0430

    Updates

--- br_bug-digit-report/service-worker.js
+++ br_bug-digit-report/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/br_bug-digit-report/precache-manifest.a327ae56c337254f904dbb4c1e08a81a.js"
+  "/br_bug-digit-report/precache-manifest.09ea8695e1687e3a25f8a0e925f2fce8.js"
 );
 
 workbox.core.skipWaiting();
