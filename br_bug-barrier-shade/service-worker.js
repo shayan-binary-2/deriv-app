22c587264bfe61b334cce2d247b5b779a6f6ffc5
shayan <shayan@firstsource.tech>
2020-06-24 10:23:41 +0430

    Updates

--- br_bug-barrier-shade/service-worker.js
+++ br_bug-barrier-shade/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/br_bug-barrier-shade/precache-manifest.44fcbbde7de8fe056c6b82d98d3e0615.js"
+  "/br_bug-barrier-shade/precache-manifest.4d721412197186fa48c87bcc979a0635.js"
 );
 
 workbox.core.skipWaiting();
