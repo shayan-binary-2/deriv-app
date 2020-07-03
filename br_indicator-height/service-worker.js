2431150b21c5f0786b06d605defbc06375a0d785
shayan <shayan@firstsource.tech>
2020-07-03 11:38:35 +0430

    Updates

--- br_indicator-height/service-worker.js
+++ br_indicator-height/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/br_indicator-height/precache-manifest.3e57d67ab9583766c746c1f4fea1635a.js"
+  "/br_indicator-height/precache-manifest.2450157fb49ef3914695ce39b83d7d3d.js"
 );
 
 workbox.core.skipWaiting();
