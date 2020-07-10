13be1892022dfaed56d5c4e34b70340989464242
shayan <shayan@firstsource.tech>
2020-07-10 13:52:20 +0430

    Updates

--- br_market-selector-scroll/service-worker.js
+++ br_market-selector-scroll/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/br_market-selector-scroll/precache-manifest.f755dcae5539ea907f8e6c0107b6eb77.js"
+  "/br_market-selector-scroll/precache-manifest.ecd658a145cf42e6c1913340a2bbc768.js"
 );
 
 workbox.core.skipWaiting();
