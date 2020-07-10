2effee385656aabc374b54995c2a6784fcfbc160
shayan <shayan@firstsource.tech>
2020-07-10 14:12:23 +0430

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5ef48aeb9437613b36071eb35abcf276.js"
+  "/precache-manifest.9654ea87540ea906b2ceccc042eb8ec8.js"
 );
 
 workbox.core.skipWaiting();
