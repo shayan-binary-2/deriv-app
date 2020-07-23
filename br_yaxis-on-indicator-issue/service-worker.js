8a48623cc82e5f115f6527d524a2a1bc8369c50f
shayan <shayan@firstsource.tech>
2020-07-23 08:51:00 +0430

    Updates

--- br_yaxis-on-indicator-issue/service-worker.js
+++ br_yaxis-on-indicator-issue/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/br_yaxis-on-indicator-issue/precache-manifest.82280e50a54ee36c63f5942e3492868f.js"
+  "/br_yaxis-on-indicator-issue/precache-manifest.cf7eea153453cd713f5daa133bd05194.js"
 );
 
 workbox.core.skipWaiting();
