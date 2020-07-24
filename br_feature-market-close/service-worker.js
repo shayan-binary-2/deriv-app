43b6b3db3aee8c6d8423ad8ea781bc6d4abf261c
shayan <shayan@firstsource.tech>
2020-07-24 14:18:23 +0430

    Updates

--- br_feature-market-close/service-worker.js
+++ br_feature-market-close/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/br_feature-market-close/precache-manifest.aa30a0bbae0e1c841dc451d543cb9c82.js"
+  "/br_feature-market-close/precache-manifest.a26752168c42885559558db21deb4f45.js"
 );
 
 workbox.core.skipWaiting();
