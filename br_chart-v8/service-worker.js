5cf973411b0df57fe20d2f210e4c3dd69b59168f
shayan <shayan@firstsource.tech>
2020-08-17 08:23:36 +0430

    Updates

--- br_chart-v8/service-worker.js
+++ br_chart-v8/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/br_chart-v8/precache-manifest.90ab59d32937db02b4730772df8720df.js"
+  "/br_chart-v8/precache-manifest.9790d6113fed5ca562d348ddb2bca8b9.js"
 );
 
 workbox.core.skipWaiting();
