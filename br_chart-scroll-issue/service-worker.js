ea1fdfd29a2bf1c6e718232e4a31c873f41ddaa8
shayan <shayan@firstsource.tech>
2020-11-30 13:31:32 +0330

    Updates

--- br_chart-scroll-issue/service-worker.js
+++ br_chart-scroll-issue/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/br_chart-scroll-issue/precache-manifest.0e04d2a9a69ca8515cd8013c71c115ce.js"
+  "/br_chart-scroll-issue/precache-manifest.84147f64f5cf7083e7d4f3e15276f61b.js"
 );
 
 workbox.core.skipWaiting();
