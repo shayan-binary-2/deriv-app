2cf2149fd22f2473444012fbae11f228db3e101b
shayan <shayan@firstsource.tech>
2021-02-04 13:54:33 +0330

    Updates

--- br_chart-scale-refactoring/service-worker.js
+++ br_chart-scale-refactoring/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/br_chart-scale-refactoring/precache-manifest.edbe038c6007c94ed3d96fe8b16b198b.js"
+  "/br_chart-scale-refactoring/precache-manifest.895d1905359a3e56e4ca3034780932e3.js"
 );
 
 workbox.core.skipWaiting();
