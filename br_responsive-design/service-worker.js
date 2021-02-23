5f9af28909f47a2403c1966f364ba474a6b753df
shayan <shayan@firstsource.tech>
2021-02-23 10:12:03 +0330

    Updates

--- br_responsive-design/service-worker.js
+++ br_responsive-design/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/br_responsive-design/precache-manifest.b9f9dbafbb03807b0b45eb1be741a48a.js"
+  "/br_responsive-design/precache-manifest.b461e6dc514f0a021589ebdde83827b7.js"
 );
 
 workbox.core.skipWaiting();
