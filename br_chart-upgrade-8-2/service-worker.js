791868a749a7d71155489fabd815eb4388904d6d
shayan <shayan@firstsource.tech>
2021-02-24 13:51:43 +0330

    Updates

--- br_chart-upgrade-8-2/service-worker.js
+++ br_chart-upgrade-8-2/service-worker.js
@@ -716,7 +716,7 @@ define("./service-worker.js",['./workbox-972bde33'], function (workbox) { 'use s
     "url": "/js/core.main~components_src_components_l.cc0aa7ea38b221fad760.js",
     "revision": null
   }, {
-    "url": "/js/core.main~s.3c70d63c135b75758da9.js",
+    "url": "/js/core.main~s.da69981836b5317e88d4.js",
     "revision": null
   }, {
     "url": "/js/core.reality-check-modal.817831968299afdaf47c.js",
