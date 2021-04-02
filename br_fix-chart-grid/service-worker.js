c6df9d11cd38e0a0dbd02688b6e58825ad374716
shayan <shayan@firstsource.tech>
2021-04-02 10:40:04 +0430

    Updates

--- br_fix-chart-grid/service-worker.js
+++ br_fix-chart-grid/service-worker.js
@@ -746,7 +746,7 @@ define("./service-worker.js",['./workbox-c5a866fd'], function (workbox) { 'use s
     "url": "/br_fix-chart-grid/js/core.main~components_src_components_l.d20b67af2a50561d2116.js",
     "revision": null
   }, {
-    "url": "/br_fix-chart-grid/js/core.main~s.05db19b6a4a22d9057e3.js",
+    "url": "/br_fix-chart-grid/js/core.main~s.a755925394b74093a96f.js",
     "revision": null
   }, {
     "url": "/br_fix-chart-grid/js/core.reality-check-modal.042f184a060be537e55c.js",
