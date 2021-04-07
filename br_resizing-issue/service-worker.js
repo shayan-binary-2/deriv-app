f7f847617d3c4a014dbfea6ff0b888ed36c3e0d2
shayan <shayan@firstsource.tech>
2021-04-07 07:38:01 +0430

    Updates

--- br_resizing-issue/service-worker.js
+++ br_resizing-issue/service-worker.js
@@ -746,7 +746,7 @@ define("./service-worker.js",['./workbox-c5a866fd'], function (workbox) { 'use s
     "url": "/br_resizing-issue/js/core.main~components_src_components_l.2d208790acb870af0d01.js",
     "revision": null
   }, {
-    "url": "/br_resizing-issue/js/core.main~s.36392df48720ffefabdf.js",
+    "url": "/br_resizing-issue/js/core.main~s.9bdf047350f54910ddda.js",
     "revision": null
   }, {
     "url": "/br_resizing-issue/js/core.reality-check-modal.042f184a060be537e55c.js",
