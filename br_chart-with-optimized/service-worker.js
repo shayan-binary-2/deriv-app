910aa54a364d959aafdffd93c4319fe891d1baa3
shayan <shayan@firstsource.tech>
2021-04-11 11:56:47 +0430

    Updates

--- br_chart-with-optimized/service-worker.js
+++ br_chart-with-optimized/service-worker.js
@@ -746,7 +746,7 @@ define("./service-worker.js",['./workbox-c5a866fd'], function (workbox) { 'use s
     "url": "/br_chart-with-optimized/js/core.main~components_src_components_l.2d208790acb870af0d01.js",
     "revision": null
   }, {
-    "url": "/br_chart-with-optimized/js/core.main~s.5da9c4e3a3b9daf29f37.js",
+    "url": "/br_chart-with-optimized/js/core.main~s.fd82f6849b4f45d648c2.js",
     "revision": null
   }, {
     "url": "/br_chart-with-optimized/js/core.reality-check-modal.042f184a060be537e55c.js",
